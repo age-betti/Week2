5ef8c762360ab6bbb44117b21e8002eb3bb20b37
age-betti <38092079+age-betti@users.noreply.github.com>
2018-04-05 13:23:19 +1000

    Add files via upload

---
+++ OOExercise1week2/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOExercise1week2_101644658
+{
+    class Student:Person
+    {
+        private int studentID;
+        public int StudentID
+        {
+            get { return studentID; }
+            set { studentID = value; }
+        }
+        public Student(string pFName, string pLName, string pGender, int pStudentID): base(pFName,pLName,pGender)
+        {
+            studentID = pStudentID;
+        }
+        public override string Work()
+        {
+            return "Study, Drink coffee, Party, Sleep, Repeat";
+        }
+    }
+}
---
+++ OOExercise1week2/TeachSpecialNeeds.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOExercise1week2_101644658
+{
+    class TeachSpecialNeeds:ITeach
+    {
+        public string Teach()
+        {
+            return "Teach special needs";
+        }
+    }
+}
